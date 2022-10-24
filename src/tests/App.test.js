34dfabebffa28e907b75a6183b7387675b746f07
matheus <mathdiogenes@gmail.com>
2022-10-24 17:27:41 -0300

    Requisito 6

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,7 +5,7 @@ import App from '../App';
 import Provider from '../context/Provider';
 
 describe('testes da página de Login', () => {
-  it('', () => {
+  it('testa se a botão de login renderiza desabilitado e se torna habilitado depois de digitar e-mail e senha válidos', () => {
     render(
       <Provider>
         <App />
@@ -22,11 +22,20 @@ describe('testes da página de Login', () => {
 
     expect(buttonSubmit).toBeEnabled();
   });
-});
 
-// test('Farewell, front-end', () => {
-//   // Este arquivo pode ser modificado ou deletado sem problemas
-//   render(<App />);
-//   const linkElement = screen.getByText(/TRYBE/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+  it('testa se após clicar no botão de login, o e-mail é salvo no localStorage e a página é redicionada para "/meals"', () => {
+    render(
+      <Provider>
+        <App />
+      </Provider>,
+    );
+
+    const emailInput = screen.getByRole('textbox', { name: /e-mail/i });
+    const passwordInput = screen.getByRole('textbox', { name: /senha/i });
+    const buttonSubmit = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(emailInput, 'teste@teste.com');
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(buttonSubmit);
+  });
+});
