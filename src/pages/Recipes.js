1075e7b810fc0f0452e9548cfe16944469fe9fd0
matheus <mathdiogenes@gmail.com>
2022-10-25 19:29:04 -0300

    Requisito 14

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import uuid from 'react-uuid';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -7,10 +8,16 @@ import Context from '../context/Context';
 function Recipes({ title, header, history, footer }) {
   const { resultKey, resultsData } = useContext(Context);
 
+  const [firstRecipes, setfirstRecipes] = useState([]);
+
   useEffect(() => {
     if (resultsData.length === 1) {
       const url = `/${resultKey[0]}/${resultsData[0][resultKey[1]]}`;
       history.push(url);
+    } else if (resultsData.length > 1) {
+      const recipesLimit = 12;
+      const twelveRecipes = resultsData.filter((recipe, index) => index < recipesLimit);
+      setfirstRecipes(twelveRecipes);
     }
   }, [resultsData]);
 
@@ -21,6 +28,20 @@ function Recipes({ title, header, history, footer }) {
         profile
         search
       />}
+      <div>
+        {firstRecipes.map((recipe, index) => (
+          <div key={ uuid() } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ recipe[`str${resultKey[2]}Thumb`] }
+              alt={ recipe[`str${resultKey[2]}`] }
+              data-testid={ `${index}-card-img` }
+            />
+            <h4 data-testid={ `${index}-card-name` }>
+              {recipe[`str${resultKey[2]}`]}
+            </h4>
+          </div>
+        ))}
+      </div>
       {footer && <Footer
         drink
         meal
