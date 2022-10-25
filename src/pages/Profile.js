8e63a9298dbc429929301e5bd2fdc39972fdffd0
Anderson-Reges <andersonreges76@gmail.com>
2022-10-25 16:37:40 -0300

    Requisito 16-17-18

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Profile({ title }) {
   return (
@@ -9,6 +10,11 @@ function Profile({ title }) {
         title={ title }
         profile
         search={ false }
+      />
+      <Footer
+        title={ title }
+        drink
+        meal
       />
     </div>
   );
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-function Recipes({ title, header }) {
+function Recipes({ title, header, footer }) {
   return (
     <div>
       {header && <Header
         title={ title }
         profile
         search
+      />}
+      {footer && <Footer
+        drink
+        meal
       />}
     </div>
   );
