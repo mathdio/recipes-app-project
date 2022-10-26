d2cc916081b75977ce23fb95dc82f53862454ea4
Anderson-Reges <andersonreges76@gmail.com>
2022-10-26 15:59:19 -0300

    Requisito-20

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) {
-  const [isLoading, setIsLoading] = useState(false);
   const [dataMeals, setDataMeals] = useState();
   const [dataDrinks, setDataDrinks] = useState([]);
   const [email, setEmail] = useState('');
@@ -13,9 +12,50 @@ function Provider({ children }) {
   const [filterValue, setFilterValue] = useState('');
   const [resultKey, setResultKey] = useState([]);
   const [resultsData, setResultsData] = useState([]);
+  const [mealsCategories, setMealsCategories] = useState();
+  const [drinksCategories, setDrinksCategories] = useState();
+
+  const getDataMeals = async () => {
+    const DOZE = 12;
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    const newdata = data.meals;
+    const filter = newdata.filter((_meal, index) => index < DOZE);
+    setDataMeals(filter);
+  };
+
+  const getDataDrinks = async () => {
+    const DOZE = 12;
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    const data = await response.json();
+    const newdata = data.drinks;
+    const filter = newdata.filter((_drink, index) => index < DOZE);
+    setDataDrinks(filter);
+  };
+
+  const getMealsCatogories = async () => {
+    const CINCO = 5;
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const data = await response.json();
+    const newdata = data.meals;
+    const filter = newdata.filter((_meals, index) => index < CINCO);
+    setMealsCategories(filter);
+  };
+
+  const getDrinksCatogories = async () => {
+    const CINCO = 5;
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+    const data = await response.json();
+    const newdata = data.drinks;
+    const filter = newdata.filter((_drinks, index) => index < CINCO);
+    setDrinksCategories(filter);
+  };
 
   const contextValue = useMemo(() => ({
-    isLoading,
+    mealsCategories,
+    drinksCategories,
+    getDrinksCatogories,
+    getMealsCatogories,
     dataMeals,
     dataDrinks,
     email,
@@ -32,29 +72,14 @@ function Provider({ children }) {
     setResultKey,
     resultsData,
     setResultsData,
-  }), [dataDrinks, dataMeals, email, isLoading,
-    password, submitDisabled,
-    searchRecipe, filterValue,
-    resultKey, resultsData]);
-
-  const getDataMeals = async () => {
-    const DOZE = 12;
-    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-    const data = await response.json();
-    const newdata = data.meals;
-    const filter = newdata.filter((meal, index) => (index < DOZE ? meal : false));
-    setDataMeals(filter);
-  };
-
-  const getDataDrinks = async () => {
-    const DOZE = 12;
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-    const data = await response.json();
-    const newdata = data.drinks;
-    const filter = newdata.filter((meal, index) => (index < DOZE ? meal : false));
-    setIsLoading(true);
-    setDataDrinks(filter);
-  };
+    getDataMeals,
+    getDataDrinks,
+  }), [mealsCategories, drinksCategories,
+    dataMeals, dataDrinks,
+    email, password,
+    submitDisabled, searchRecipe,
+    filterValue, resultKey,
+    resultsData]);
 
   useEffect(() => {
     const RegEx = /\S+@\S+\.\S+/;
@@ -65,8 +90,6 @@ function Provider({ children }) {
     } else {
       setSubmitDisabled(true);
     }
-    getDataMeals();
-    getDataDrinks();
   }, [email, password]);
 
   return (
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,19 +1,40 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import uuid from 'react-uuid';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Context from '../context/Context';
 import CardRecipes from '../components/CardRecipes';
+import CardCategory from '../components/CardCategory';
 
 function Recipes({ title, header, history, footer }) {
-  const { resultKey, resultsData, dataDrinks, dataMeals } = useContext(Context);
+  const { resultKey, resultsData, dataDrinks, dataMeals,
+    getDataMeals, getDataDrinks, getDrinksCatogories,
+    getMealsCatogories, mealsCategories,
+    drinksCategories } = useContext(Context);
+  const [firstRecipes, setfirstRecipes] = useState([]);
+  const [firstRender, setFirstRender] = useState(true);
 
   useEffect(() => {
     if (resultsData.length === 1) {
       const url = `/${resultKey[0]}/${resultsData[0][resultKey[1]]}`;
       history.push(url);
+    } else if (resultsData.length > 1) {
+      const recipesLimit = 12;
+      const twelveRecipes = resultsData.filter((_recipe, index) => index < recipesLimit);
+      setfirstRecipes(twelveRecipes);
+      setFirstRender(false);
     }
   }, [history, resultKey, resultsData]);
+
+  useEffect(() => {
+    getDataMeals();
+    getDataDrinks();
+    getDrinksCatogories();
+    getMealsCatogories();
+    // console.log(dataMeals);
+    // console.log(dataDrinks);
+  }, []);
 
   return (
     <div>
@@ -22,20 +43,41 @@ function Recipes({ title, header, history, footer }) {
         profile
         search
       />}
-      {
-        title === 'Meals' ? (
-          <CardRecipes
-            dataMeals={ dataMeals }
-            header
-          />
+      <div>
+        {firstRecipes.map((recipe, index) => (
+          <div key={ uuid() } data-testid={ `${index}-recipe-card` }>
+            <img
+              src={ recipe[`str${resultKey[2]}Thumb`] }
+              alt={ recipe[`str${resultKey[2]}`] }
+              data-testid={ `${index}-card-img` }
+            />
+            <h4 data-testid={ `${index}-card-name` }>
+              {recipe[`str${resultKey[2]}`]}
+            </h4>
+          </div>
+        ))}
+      </div>
+      {firstRender
+        && (title === 'Meals' ? (
+          <div>
+            <CardCategory
+              mealsCategories={ mealsCategories }
+            />
+            <CardRecipes
+              dataMeals={ dataMeals }
+            />
+          </div>
         )
           : (
-            <CardRecipes
-              dataDrinks={ dataDrinks }
-              header
-            />
-          )
-      }
+            <div>
+              <CardCategory
+                drinksCategories={ drinksCategories }
+              />
+              <CardRecipes
+                dataDrinks={ dataDrinks }
+              />
+            </div>
+          ))}
       {footer && <Footer
         drink
         meal
