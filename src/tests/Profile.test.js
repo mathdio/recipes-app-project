1d3f7ee704224e1e332ce5f9e520f9fb0fa57389
André Gross <andgross@gmail.com>
2022-10-26 10:51:01 -0300

    pausa para resolver conflitos em outro req

---
+++ src/tests/Profile.test.js
@@ -0,0 +1,10 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+
+describe('Testa a tela do componente Profile', () => {
+  test('', () => {
+
+  });
+});
