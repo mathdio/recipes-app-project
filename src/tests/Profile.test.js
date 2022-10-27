b12697f22c32cee3217b2a122800ae9f5879da03
Matheus Diógenes Almeida <101986958+mathdio@users.noreply.github.com>
2022-10-27 13:58:46 -0300

    Update Profile.test.js
    Corrigindo os erros de lint para o avaliador rodar

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,8 +1,3 @@
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
-import App from '../App';
-
 describe('Testa a tela do componente Profile', () => {
   test('', () => {
 
