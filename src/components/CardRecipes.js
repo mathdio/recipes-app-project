1478440c6e9baeeadfc0aa8748d23aa91a23bbfc
matheus <mathdiogenes@gmail.com>
2022-10-31 15:25:51 -0300

    testes do Header (req9)

--- src/components/CardRecipes.js
+++ src/components/CardRecipes.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom';
 
 export default function CardRecipes({ dataMeals, dataDrinks }) {
-  console.log(dataDrinks);
   return (
     <div>
       {dataMeals
