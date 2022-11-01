d950ebf4c6b7713939c4562306a2dc461c767f17
matheus <mathdiogenes@gmail.com>
2022-11-01 19:48:00 -0300

    resoluções de erros do lint

--- src/tests/FavoriteRecipes.test.js
+++ src/tests/FavoriteRecipes.test.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import { waitFor, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouterAndContext from './helpers/renderWithRouterAndContext';
 
 const FAVORITE_BTN = '1-horizontal-favorite-btn';
 const FAVORITE_PAGE_PATHNAME = '/favorite-recipes';
+const favoriteRecipesArray = [
+  {
+    id: '15997',
+    type: 'drink',
+    nationality: '',
+    category: 'Ordinary Drink',
+    alcoholicOrNot: 'Optional alcohol',
+    name: 'GG',
+    image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+  },
+  {
+    id: '52977',
+    type: 'meal',
+    nationality: 'Turkish',
+    category: 'Side',
+    alcoholicOrNot: '',
+    name: 'Corba',
+    image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+  },
+];
 
 it('testa se ao clicar no botão de desfavoritar, a receita é retirada da lista de favoritos', async () => {
-  const favoriteRecipesArray = [
-    {
-      id: '15997',
-      type: 'drink',
-      nationality: '',
-      category: 'Ordinary Drink',
-      alcoholicOrNot: 'Optional alcohol',
-      name: 'GG',
-      image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-    },
-    {
-      id: '52977',
-      type: 'meal',
-      nationality: 'Turkish',
-      category: 'Side',
-      alcoholicOrNot: '',
-      name: 'Corba',
-      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-    },
-  ];
   localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipesArray));
   renderWithRouterAndContext(<App />, FAVORITE_PAGE_PATHNAME);
 
@@ -35,30 +35,21 @@ it('testa se ao clicar no botão de desfavoritar, a receita é retirada da lista
   userEvent.click(favoriteBtns[0]);
 });
 
-it('testa se ao clicar no botão de desfavoritar, a receita é retirada da lista de favoritos', async () => {
-  const favoriteRecipesArray = [
-    {
-      id: '15997',
-      type: 'drink',
-      nationality: '',
-      category: 'Ordinary Drink',
-      alcoholicOrNot: 'Optional alcohol',
-      name: 'GG',
-      image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
-    },
-    {
-      id: '52977',
-      type: 'meal',
-      nationality: 'Turkish',
-      category: 'Side',
-      alcoholicOrNot: '',
-      name: 'Corba',
-      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-    },
-  ];
+it('testa se as comidas são filtradas ao clicar no botão', () => {
   localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipesArray));
   renderWithRouterAndContext(<App />, FAVORITE_PAGE_PATHNAME);
 
   const filterMealsBtn = screen.getByRole('button', { name: /meals/i });
   userEvent.click(filterMealsBtn);
+
+  const filterDrinksBtn = screen.getByRole('button', { name: /drinks/i });
+  userEvent.click(filterDrinksBtn);
+});
+
+it('testa se os drinks são filtrados ao clicar no botão', () => {
+  localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipesArray));
+  renderWithRouterAndContext(<App />, FAVORITE_PAGE_PATHNAME);
+
+  const AllBtn = screen.getByRole('button', { name: /all/i });
+  userEvent.click(AllBtn);
 });
