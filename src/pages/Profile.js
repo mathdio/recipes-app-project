f10abd59f46149ddad6633485900ba3c98797a03
matheus <mathdiogenes@gmail.com>
2022-11-23 13:54:03 -0300

    style: inserção dos ícones nos botões da Profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import doneIcon from '../images/doneIcon.svg';
+import favoriteIcon from '../images/favoriteIcon.svg';
+import logoutIcon from '../images/logoutIcon.svg';
+import './Profile.css';
 
 function Profile({ title, history }) {
   const email = localStorage.getItem('user')
@@ -23,24 +27,62 @@ function Profile({ title, history }) {
         profile
         search={ false }
       />
-      <p data-testid="profile-email">
-        Email:
-        {' '}
-        {email.email}
-      </p>
-      <Link to="/done-recipes">
-        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      </Link>
-      <Link to="/favorite-recipes">
-        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      </Link>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ handleClickLogout }
-      >
-        Logout
-      </button>
+      <main className="Profile__buttons-container">
+        <p data-testid="profile-email">
+          Email:
+          {' '}
+          {email.email}
+        </p>
+        <Link to="/done-recipes">
+          <label htmlFor="profile-done-btn">
+            <input
+              id="profile-done-btn"
+              type="image"
+              alt=""
+              src={ doneIcon }
+              data-testid="profile-done-btn"
+              className="profile-page-icons"
+            />
+            Done Recipes
+          </label>
+          {/* <button type="button" data-testid="profile-done-btn">Done Recipes</button> */}
+        </Link>
+        <Link to="/favorite-recipes">
+          <label htmlFor="profile-favorite-btn">
+            <input
+              id="profile-favorite-btn"
+              type="image"
+              alt=""
+              src={ favoriteIcon }
+              data-testid="profile-favorite-btn"
+              className="profile-page-icons"
+            />
+            Favorite Recipes
+          </label>
+          {/* <button type="button" data-testid="profile-favorite-btn">
+            Favorite Recipes
+          </button> */}
+        </Link>
+        <label htmlFor="profile-logout-btn">
+          <input
+            id="profile-logout-btn"
+            type="image"
+            alt=""
+            src={ logoutIcon }
+            data-testid="profile-logout-btn"
+            className="profile-page-icons"
+            onClick={ handleClickLogout }
+          />
+          Logout
+        </label>
+        {/* <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleClickLogout }
+        >
+          Logout
+        </button> */}
+      </main>
       <Footer
         title={ title }
         drink
